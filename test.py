537b88fd21a0d9e44ec938f6213d30c67880c516
Exmoment <139344527+Exmoment@users.noreply.github.com>
2023-08-08 15:02:33 +0300

    Add files via upload

--- test.py
+++ test.py
@@ -1,2 +1,22 @@
-import tkinter
+from tkinter import *
+from objects import objects
 
+'''
+def clicked():
+    text.configure(text = '123')
+        
+#    objects.objects
+'''
+
+window = Tk()
+window.title("Тест")
+window.geometry('350x100')
+text = Label(window, text = "Редиска!", font = ("Arial", 36))
+#text.grid(row = 0, column = 0)
+text.pack()
+btn = Button(window, text = "Запуск программы", command = objects)
+btn.pack()
+
+
+
+window.mainloop()
