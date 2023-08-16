c19048226a9cbf44aaf9211f56822dded47cb172
Exmoment <139344527+Exmoment@users.noreply.github.com>
2023-08-16 17:31:33 +0300

    Add files via upload

--- test.py
+++ test.py
@@ -10,7 +10,6 @@ def createGETandPOST():
     GET_POST.iterrationGET = int(iterTest_entry.get())
     GET_POST.iterrationPOST = int(iterTest_entry.get())
     GET_POST.POSTandGET()
-    print(iterTest_entry.get())
 
 
 def createPOST():
@@ -20,14 +19,27 @@ def createPOST():
         POST.pathBody = bodyTest_path.get()
         POST.pathAmmo = ammoTest_path.get()
         POST.POST()
-        print(iterTest_entry.get())
 
+    else:
+        messagebox.showinfo('Error', 'В поле итераций должно быть введено значение больше нуля')
+
+
+def createGET():
+    if  ((iterTest_entry.get().isdigit()) > 0) and (int(iterTest_entry.get()) > 0):
+        GET = functions.choiceType('GET')
+        GET.iterrationGET = int(iterTest_entry.get())
+        GET.pathBody = bodyTest_path.get()
+        GET.GET()
     else:
         messagebox.showinfo('Error', 'В поле итераций должно быть введено значение больше нуля')
 
 
 def destroyPOSTreq():
     POSTreq.destroy()
+
+
+def destroyGETreq():
+    GETreq.destroy()
 
 
 def clicked():
@@ -37,6 +49,7 @@ def clicked():
     global ammoTest_path
     global bodyTest_path
     global POSTreq
+    global GETreq
 
     if ('GET and POST' in typeLoad):
         tests_iter = Label(window, text = "Введите количество итераций тестов:", font = ("Arial", 12))
@@ -47,18 +60,17 @@ def clicked():
 
     elif ('POST' in typeLoad):
         start.destroy()
-        window.geometry('650x150')
         POSTreq = Frame(relief = FLAT)
         POSTreq.pack(pady = [5, 5])
         iterTest = Label(master = POSTreq, text = "Количество итераций тестов:", font = ("Arial", 12))
         iterTest.grid(column = 0, row = 0, padx = [5, 5], pady = [5, 5], sticky = 'e')
         iterTest_entry = Entry(master = POSTreq, width = 25)
         iterTest_entry.grid(column = 1, row = 0, padx = [5, 5], pady = [5, 5])
-        bodyTest = Label(master = POSTreq, text = "Путь к файлу конфигурации:", font = ("Arial", 12))
+        bodyTest = Label(master = POSTreq, text = "Путь или имя файла конфигурации:", font = ("Arial", 12))
         bodyTest.grid(column = 0, row = 1, padx = [5, 5], pady = [5, 5], sticky = 'e')
         bodyTest_path = Entry(master = POSTreq, width = 25)
         bodyTest_path.grid(column = 1, row = 1, padx = [5, 5], pady = [5, 5])
-        ammoTest = Label(master = POSTreq, text = "Путь к файлу запросов:", font = ("Arial", 12))
+        ammoTest = Label(master = POSTreq, text = "Путь или имя файла запросов:", font = ("Arial", 12))
         ammoTest.grid(column = 0, row = 2, padx = [5, 5], pady = [5, 5], sticky = 'e')
         ammoTest_path = Entry(master = POSTreq, width = 25)
         ammoTest_path.grid(column = 1, row = 2, padx = [5, 5], pady = [5, 5])
@@ -67,19 +79,26 @@ def clicked():
         back_btn = Button(master = POSTreq, text = 'Вернуться к выбору запросов', command = lambda:[destroyPOSTreq(), wiget_dontgenerate()])
         back_btn.grid(column = 0, row = 3, padx = 5, pady = 5)
         print('Ожидание ввода параметров')
+        window.geometry('550x150')
 
     elif ('GET' in typeLoad):
+        start.destroy()
         GETreq = Frame(relief = FLAT)
         GETreq.pack(pady = [5, 5])
         tests_iter = Label(master = GETreq, text = "Введите количество итераций тестов:", font = ("Arial", 12))
         tests_iter.grid(column = 0, row = 0, padx = [5, 5], pady = [5, 5], sticky = 'e')
         tests_iter_entry = Entry(master = GETreq, width = 25)
         tests_iter_entry.grid(column = 1, row = 0, padx = [5, 5], pady = [5, 5])
-        tests_config = Label(master = GETreq, text = "Введите путь к файлу конфигурации:", font = ("Arial", 12))
+        tests_config = Label(master = GETreq, text = "Путь или имя файла конфигурации:", font = ("Arial", 12))
         tests_config.grid(column = 0, row = 1, padx = [5, 5], pady = [5, 5], sticky = 'e')
         tests_config_path = Entry(master = GETreq, width = 25)
         tests_config_path.grid(column = 1, row = 1, padx = [5, 5], pady = [5, 5])
-        window.geometry('650x450')
+        iterTest_btn = Button(master = GETreq, text = 'Принять', command = createGET)
+        iterTest_btn.grid(column = 1, row = 2, padx = 5, pady = 5)
+        back_btn = Button(master = GETreq, text = 'Вернуться к выбору запросов', command = lambda:[destroyGETreq(), wiget_dontgenerate()])
+        back_btn.grid(column = 0, row = 2, padx = 5, pady = 5)
+        print('Ожидание ввода параметров')
+        window.geometry('550x120')
 
     else:
         messagebox.showinfo('Ошибка ввода', 'Вы не выбрали тип запросов, попробуйте еще раз')
@@ -93,7 +112,8 @@ def wiget_dontgenerate():
         type_req = Label(master = start, text = "Выберите тип запросов:", font = ("Arial", 12))
         type_req.pack(pady = [5, 5])
         tupeTest = ("GET", "POST", "GET and POST")
-        btn1 = Button(master = start, text = 'Выбрать', command = clicked).pack(side = BOTTOM, fill = X, padx = 5, pady = 5)
+        choice_btn = Button(master = start, text = 'Выбрать', command = clicked)
+        choice_btn.pack(side = BOTTOM, fill = X, padx = 5, pady = 5)
         choice_tupe = Combobox(master = start, values = tupeTest, width = 25)
         choice_tupe.current()
         choice_tupe.pack(pady = [5, 5])
